aab04763f573cd7a9f75e5efe1dd42e340705673
Fireguy13 <fow21010@byui.edu>
2022-10-30 15:07:20 -0600

    week7

---
+++ lesson7/scripts/main.js
@@ -0,0 +1,41 @@
+//Menu Hamburger ------------------------------------------------------
+function toggleMenu(){
+    document.getElementById('navigation').classList.toggle("open");
+  }
+  
+  const x = document.getElementById('hambBtn')
+  x.onclick = toggleMenu;
+  
+  
+  
+  // set current year in footer ............................................................
+  const currentDate = new Date();
+  document.querySelector('#year').textContent = currentDate.getFullYear();
+  
+  // set current year in header
+  document.querySelector('#timeHeader').textContent = currentDate.getFullYear();
+  
+  // last modified
+  const dateHeader = document.querySelector('#timeHeader');
+  
+  const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
+  
+  dateHeader.innerHTML = `<em>${fulldateUK}</em>`;
+  
+  document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+  
+  //Banner ------------------------------------------------------------------------------
+  let bannerDay = new Date().getDay();
+  
+  const banner = document.querySelector('#banner');
+  if (bannerDay === 1 || bannerDay === 2){
+      banner.style.display = 'block';
+  } else{
+      banner.style.display = 'none';
+  }
+  
+  const close = document.querySelector('#close');
+  
+  close.addEventListener('click', () => {
+      banner.style.display = 'none';
+  })
---
+++ lesson7/scripts/main2.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open")
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu
+
+// this is the upday time
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
